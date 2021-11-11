22a45996169040382d383afa3597e47b53a3f6c3
“yourGithubUsername” <your@emailadd.com>
2021-11-11 15:53:56 +0000

    add main.js

--- js/index.js
+++ js/index.js
@@ -2,22 +2,6 @@
 
 
 
-//click timeline for experience
-// const datesLi = document.querySelectorAll('.datesLi');
-// const exTitle = document.getElementById('exTitle');
-// const years = document.getElementById('years');
-// const exPara = document.getElementById('exPara');
-
-// datesLi.forEach(period => {
-//     period.addEventListener('click', e => {
-//         exTitle.textContent = e.target.getAttribute('value');
-//     });
-// });
-
-
-//for 2018 experience
-
-
 
 //hover tech over
 const techContainer = document.querySelectorAll('.techContainer');
